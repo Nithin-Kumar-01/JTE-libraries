52dd6c7061ad1bf42925740cbb41da2438f878b6
Nithin-Kumar-01 <133529661+Nithin-Kumar-01@users.noreply.github.com>
2024-11-15 21:17:20 +0530

    Update astro_start.groovy

--- libraries/Astronomer_start/steps/astro_start.groovy
+++ libraries/Astronomer_start/steps/astro_start.groovy
@@ -2,6 +2,8 @@ void call(){
     node
     {
             stage("Astronomer: Start"){
+                 sh 'cd dbt-airflow-poc' 
+                sh 'pwd'
         sh ' astro dev start'
             }
     }
