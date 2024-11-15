f23cbb4f0becc7ab20fa60c50ce295f584e59fa4
Nithin-Kumar-01 <133529661+Nithin-Kumar-01@users.noreply.github.com>
2024-11-15 18:38:55 +0530

    Delete astro_build/steps directory

--- astro_build/steps/astro_build.groovy
+++
@@ -1,8 +0,0 @@
-void call(){
-    stage("Astronomer: Build"){
-        sh '''
-               curl -sSL install.astronomer.io | sudo bash -s -- v1.30.0
-               astro version
-        '''
-    }
-}
