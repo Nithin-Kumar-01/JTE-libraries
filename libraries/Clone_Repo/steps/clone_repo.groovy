1b7fdeb3e72ae126ddf2eb6b020bb716484853b1
Nithin-Kumar-01 <133529661+Nithin-Kumar-01@users.noreply.github.com>
2024-11-15 21:16:53 +0530

    Update clone_repo.groovy

--- libraries/Clone_Repo/steps/clone_repo.groovy
+++ libraries/Clone_Repo/steps/clone_repo.groovy
@@ -3,7 +3,7 @@ void call(){
                 stage("Project: clone"){
                 
         sh 'git clone https://github.com/Nithin-Kumar-01/dbt-airflow-poc.git'
-        sh 'cd dbt-airflow-poc' 
+       
         }
         }   
 }
