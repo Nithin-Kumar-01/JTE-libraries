e3b37cd4cd9b600e0c6d9293c8723c86f8d3e427
Nithin-Kumar-01 <133529661+Nithin-Kumar-01@users.noreply.github.com>
2024-11-15 21:09:51 +0530

    Update astro_build.groovy

--- libraries/Astronomer/steps/astro_build.groovy
+++ libraries/Astronomer/steps/astro_build.groovy
@@ -1,8 +1,10 @@
 def call(){
+    node {
     stage("Astronomer: Build"){
         sh '''
                curl -sSL install.astronomer.io | sudo bash -s -- v1.30.0
                astro version
         '''
+    }
     }
 }
